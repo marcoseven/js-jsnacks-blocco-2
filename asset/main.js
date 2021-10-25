85ca067af435d1bc3c1976ecfb6947bdbcd19582
Marcoseven <marco.caliciotti3@gmail.com>
2021-10-25 12:24:43 +0200

    Aggiunte modifiche file javascript - 2° live coding
    Inserita traccia esercizio 2° live coding, un array con oggetti e i vari commenti.

--- asset/main.js
+++ asset/main.js
@@ -5,7 +5,7 @@ Stampare a schermo la bici con peso minore.
 */
 
 // Array di oggetti
-const bicycle = [
+/* const bicycle = [
 	{
 		nome: "MERIDA",
 		peso: "6.5 kg",
@@ -31,11 +31,49 @@ const bicycle = [
 		peso: "9.5 kg",
 	},
 ];
-
+ */
 // ciclo for in
-for (let key in bicycle) {
+/* for (let key in bicycle) {
 	console.log(bicycle[key]);
 }
-
+ */
 // DOM
-const pesoBici = document.getElementById("result");
+// const pesoBici = document.getElementById("result");
+
+
+// 2° Live Coding
+/*
+Descrizione: 
+Creare un array di oggetti di squadre di calcio.
+Ogni squadra avrà diverse proprietà: nome, punti fatti e falli subiti.
+Nome sarà l’unica proprietà da compilare, mentre le altre saranno tutte settate a 0. (modificato) 
+*/
+
+// Array di oggetti
+const club = [
+	{
+		nome: "Inter",
+		punti_fatti: "0",
+		falli_subiti: "0",
+	},
+	{
+		nome: "Milan",
+		punti_fatti: "0",
+		falli_subiti: "0",
+	},
+	{
+		nome: "Juve",
+		punti_fatti: "0",
+		falli_subiti: "0",
+	},
+	{
+		nome: "Roma",
+		punti_fatti: "0",
+		falli_subiti: "0",
+	},
+	{
+		nome: "Lazio",
+		punti_fatti: "0",
+		falli_subiti: "0",
+	},
+];
